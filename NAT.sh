2205ce3553074ba89e38f79b91a0b58adde311bc
JustinSinc <jpsinkula@hotmail.com>
2015-04-05 00:19:19 -0500

    Update NAT.sh

--- NAT.sh
+++ NAT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "What is the port you would like to forward?"
-read port_in 
-echo "What IP would you like to forward to? (192.168.122.x)"
+read port_in
+echo "What IP would you like to forward to?"
 read ip
-iptables -t nat -A PREROUTING -p TCP --dport $port_in -j DNAT --to-destination $ip:22
+iptables -t nat -I PREROUTING -p tcp --dport $port_in -j DNAT --to-destination $ip:22
 iptables -I FORWARD -m state -d 192.168.122.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
