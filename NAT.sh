bc7ffd361a32e51f456e25e97f7d7ada8695439e
JustinSinc <jpsinkula@hotmail.com>
2016-03-09 17:49:50 -0600

    Delete NAT.sh

--- NAT.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "What is the port you would like to forward?"
-read port_in
-echo "What IP would you like to forward to?"
-read ip
-iptables -t nat -I PREROUTING -p tcp --dport $port_in -j DNAT --to-destination $ip:22
-iptables -I FORWARD -m state -d 192.168.122.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
