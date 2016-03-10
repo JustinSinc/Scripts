a41b17313db433be7b68c56371a798b73307a349
JustinSinc <jpsinkula@hotmail.com>
2016-03-09 18:01:56 -0600

    Delete backup-weekly.sh

--- backup-weekly.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Copy the most recent day's backups to remote backup server
-rsync -avuh --ignore-existing --delete -e "ssh -p 443" --partial /storage/backups/$(date +%Y%m%d)/*.tgz storage:/backups/
