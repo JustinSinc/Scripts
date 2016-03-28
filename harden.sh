c57f96d46c01b89e711b285f5519f5e8b13c17d7
Justin Sinc <jpsinkula@hotmail.com>
2016-03-27 20:31:42 -0500

    Update harden.sh

--- harden.sh
+++ harden.sh
@@ -2,4 +2,4 @@
 ssh-copy-id $1
 scp ~/ansible/configs/sshd_config $1:~/sshd_config
 ssh -t $1 "sudo mv ~/sshd_config /etc/sshd_config && sudo /bin/rm -v /etc/ssh/ssh_host_* && sudo dpkg-reconfigure openssh-server && echo 'sshd: 192.184.82.89' | sudo tee /etc/hosts.allow && echo 'ALL:ALL' | sudo tee /etc/hosts.deny"
-ssh-keygen -f "/home/sinc/.ssh/known_hosts" -R $1
+ssh-keygen -f "~/.ssh/known_hosts" -R $1
