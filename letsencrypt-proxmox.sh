d04dd8ce308f420a9a24eec13cd5472c03840fe7
Justin Sinc <jpsinkula@hotmail.com>
2016-04-28 11:35:59 -0500

    Update letsencrypt-proxmox.sh

--- letsencrypt-proxmox.sh
+++ letsencrypt-proxmox.sh
@@ -26,14 +26,14 @@ sed -i '/ \"\"\"$/a \\n\ \ \ \ return False\n' /letsencrypt/certbot/plugins/util
 /letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges http-01 -d $host_name --agree-tos --email=$email
 
 # back up existing Proxmox certs
-mv -- /etc/pve/pve-root-ca.pem /etc/pve/pve-root-ca.pem.orig
-mv -- /etc/pve/local/pve-ssl.key /etc/pve/local/pve-ssl.key.orig
-mv -- /etc/pve/local/pve-ssl.pem /etc/pve/local/pve-ssl.pem.orig
+mv /etc/pve/pve-root-ca.pem /etc/pve/pve-root-ca.pem.orig
+mv /etc/pve/local/pve-ssl.key /etc/pve/local/pve-ssl.key.orig
+mv /etc/pve/local/pve-ssl.pem /etc/pve/local/pve-ssl.pem.orig
 
 # install new LetsEncrypt certs
-cp -- "/etc/letsencrypt/live/$host_name/chain.pem" /etc/pve/pve-root-ca.pem
-cp -- "/etc/letsencrypt/live/$host_name/privkey.pem" /etc/pve/local/pve-ssl.key
-cp -- "/etc/letsencrypt/live/$host_name/cert.pem" /etc/pve/local/pve-ssl.pem
+cp "/etc/letsencrypt/live/$host_name/chain.pem" /etc/pve/pve-root-ca.pem
+cp "/etc/letsencrypt/live/$host_name/privkey.pem" /etc/pve/local/pve-ssl.key
+cp "/etc/letsencrypt/live/$host_name/cert.pem" /etc/pve/local/pve-ssl.pem
 
 # restart Proxmox VE
 service pveproxy restart
