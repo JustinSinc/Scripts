5662c9f53931a4c8d254790c35058cf4ec23b884
Justin Sinc <jpsinkula@hotmail.com>
2016-06-13 23:47:19 -0500

    Update port-close.sh

--- port-close.sh
+++ port-close.sh
@@ -6,20 +6,20 @@ echo -e "\nProtocol:     (format tcp/udp)\n"
 read protocol
 
 # Which address were we forwarding to?
-echo -e "\nIP address to forward to:\n"
+echo -e "\nIP address forwarded to:\n"
 read forward_to
 
 # Which port were we forwarding?
-echo -e "\nPort to forward:\n"
+echo -e "\nPort forwarded:\n"
 read forwarded_port
 
 # Which port were we forwarding to?
-echo -e "\nPort to forward to:\n"
+echo -e "\nPort forwarded to:\n"
 read forwarded_to
 
 # Remove the port forward from iptables
 sudo iptables -t nat -D PREROUTING -p "$protocol" --dport "$forwarded_port" -j DNAT --to "$forward_to":"$forwarded_to"
 
 # Clear the screen, and let the user know the forward succeeded
-clear
+cleara
 echo -e "\n"$protocol" port "$forwarded_port" no longer forwarded to port "$forwarded_to" on host "$forward_to".\n\n\n"
