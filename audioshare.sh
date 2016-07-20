775260b26f0b68e02ffafa0f3c3f7cdb44c67901
Justin Sinc <jpsinkula@hotmail.com>
2016-07-20 14:44:45 -0500

    Fixed issue when no files exist

--- audioshare.sh
+++ audioshare.sh
@@ -7,23 +7,27 @@ set -euo pipefail
 rm /home/sinc/audio/*.bak 2> /dev/null
 
 # add any new mp3 files to the page
-for i in /home/sinc/audio/*.mp3; do
+for i in $(find /home/sinc/audio/*.mp3 -type f -printf "%f\n" 2> /dev/null); do
+    
     # create an HTML5 audio tag for any .mp3 audio clips
-    sed -i.bak "/<!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/mp3"> Your browser does not support the audio tag. </audio>" index.html
+    sed -i.bak "/<\!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/mp3"> Your browser does not support the audio tag. </audio>" /home/sinc/audio/index.html
 
     # move the audio file to the webroot and make it readable
     mv $i /var/www/html/audio/
     chmod 755 /var/www/html/audio/$i
+
 done
 
 # add any new ogg vorbis files to the page
-for i in /home/sinc/audio/*.ogg; do
-    # create an HTML5 audio tag for any .ogg audio clips
-    sed -i.bak "/<!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/ogg"> Your browser does not support the audio tag. </audio>" index.html
+for i in $(find /home/sinc/audio/*.ogg -type f -printf "%f\n" 2> /dev/null); do
+
+# create an HTML5 audio tag for any .ogg audio clips
+    sed -i.bak "/<\!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/ogg"> Your browser does not support the audio tag. </audio>" /home/sinc/audio/index.html
 
     # move the audio file to the webroot and make it readable
     mv $i /var/www/html/audio/
     chmod 755 /var/www/html/audio/$i
+
 done
 
 # move the updated index.html to the webroot
