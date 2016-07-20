71e4fc5b88e5c4721be8213b8e2daa0f05448ecc
Justin Sinc <jpsinkula@hotmail.com>
2016-07-20 16:03:33 -0500

    Fixed issue with failing silently on errors

--- audioshare.sh
+++ audioshare.sh
@@ -4,13 +4,13 @@
 set -euo pipefail
 
 # remove any leftover backup files, suppressing errors if none exist
-rm /home/sinc/audio/*.bak 2> /dev/null
+rm /root/audio/*.bak 2>/dev/null && echo "Removed existing backup files." || echo "No existing backup files."
 
 # add any new mp3 files to the page
-for i in $(find /home/sinc/audio/*.mp3 -type f -printf "%f\n" 2> /dev/null); do
+for i in $(find /root/audio/*.mp3 -type f -printf "%f\n" 2> /dev/null); do
 
     # create an HTML5 audio tag for any .mp3 audio clips
-    sed -i.bak "/<\!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/mp3"> Your browser does not support the audio tag. </audio>" /home/sinc/audio/index.html
+    sed -i.bak "/<\!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/mp3"> Your browser does not support the audio tag. </audio>" /root/audio/index.html
 
     # move the audio file to the webroot and make it readable
     mv "$i" /var/www/html/audio/
@@ -19,10 +19,10 @@ for i in $(find /home/sinc/audio/*.mp3 -type f -printf "%f\n" 2> /dev/null); do
 done
 
 # add any new ogg vorbis files to the page
-for i in $(find /home/sinc/audio/*.ogg -type f -printf "%f\n" 2> /dev/null); do
+for i in $(find /root/audio/*.ogg -type f -printf "%f\n" 2> /dev/null); do
 
 # create an HTML5 audio tag for any .ogg audio clips
-    sed -i.bak "/<\!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/ogg"> Your browser does not support the audio tag. </audio>" /home/sinc/audio/index.html
+    sed -i.bak "/<\!-- Begin -->/a <br />"$i"<br /><audio controls> <source src=\"/audio/$i\" type="audio/ogg"> Your browser does not support the audio tag. </audio>" /root/audio/index.html
 
     # move the audio file to the webroot and make it readable
     mv "$i" /var/www/html/audio/
@@ -31,4 +31,4 @@ for i in $(find /home/sinc/audio/*.ogg -type f -printf "%f\n" 2> /dev/null); do
 done
 
 # move the updated index.html to the webroot
-cp /home/sinc/audio/index.html /var/www/html/index.html
+cp /root/audio/index.html /var/www/html/index.html
