109e5bc271e26be7d35574c3b73ddb5d1b672ce0
Justin Sinc <jpsinkula@hotmail.com>
2017-01-13 10:05:32 -0600

    Create musicbot.sh

---
+++ musicbot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# a script to generate new discord musicbots for /r/CasualConversation kill and delete any existing musicbot instanc$
+echo "Removing any existing bot instances..."
+
+for i in "$(docker ps | grep musicbot | cut -d " " -f1)"; do
+        docker kill "$i";
+        docker rm "$i";
+        echo "Killed container "$i"...";
+done
+
+# start in user's home dir
+cd ~
+
+# create a temporary working directory
+tempdir='musicbot$(date "+%Y%m%d%H%M%S")'
+mkdir ~/"$tempdir"
+cd ~/"$tempdir"
+
+# grab the Dockerfile and configuration directory, then move into the Docker build directory
+curl http://musicbot.url/musicbotv3.tar | tar x
+cd ./musicbot
+
+# build a new docker image
+docker build -t musicbot .
+
+# remove the build directory
+cd ~
+rm -rf ~/"$tempdir"
+
+# run a new instance of musicbot in the background
+docker run -d musicbot
