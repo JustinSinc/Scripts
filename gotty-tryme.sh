cfc29bc4eac5119094290aa690c332eff52d846b
Justin Sinc <jpsinkula@hotmail.com>
2017-06-30 13:05:50 -0500

    Update gotty-tryme.sh

--- gotty-tryme.sh
+++ gotty-tryme.sh
@@ -35,4 +35,6 @@ swap_limit="128m"
 # kernel memory
 kmem_limit="64m"
 
-gotty --credential "$gotty_user":"$gotty_pass" --port "$gotty_port" -w -- docker run --interactive --tty --rm --cpus="$cpu_limit" --memory="$mem_limit" --memory-swap="$swap_limit" --kernel-memory="$kmem_limit" -- alpine:latest /bin/ash
+# create a tmux session to run in
+tmux has-session -t tryme || tmux new-session -d -s tryme
+tmux send -t tryme "gotty --credential "$gotty_user":"$gotty_pass" --port "$gotty_port" -w -- docker run --interactive --tty --rm --cpus="$cpu_limit" --memory="$mem_limit" --memory-swap="$swap_limit" --kernel-memory="$kmem_limit" -- alpine:latest /bin/ash" && tmux send-key Enter
