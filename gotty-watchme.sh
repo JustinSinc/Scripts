6003ce55fe9fed9386159b66ee2c930ce2fb1e52
Justin Sinc <jpsinkula@hotmail.com>
2017-07-03 10:39:51 -0500

    Update gotty-watchme.sh

--- gotty-watchme.sh
+++ gotty-watchme.sh
@@ -5,7 +5,7 @@
 
 # spawn new GoTTY session in a tmux session named "watchme"
 tmux has-session -t watchme || tmux new-session -d -s watchme
-tmux send -t watchme "gotty -p 1338 --title-format "Watch Me" tmux a -t watchme" Enter
+tmux send -t watchme "gotty -p 1338 --title-format "Watch Me" tmux a -t watchme" && tmux send-key Enter
 
 # connect to the tmux session locally
 tmux a -t watchme
