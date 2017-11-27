913de2e0c1ecd843b832d0c908cddcee50aa3381
Justin Sinc <jpsinkula@hotmail.com>
2017-11-27 00:42:17 -0600

    epoc_wrapper.sh

---
+++ epoc_wrapper.sh
@@ -0,0 +1,122 @@
+#!/bin/rzsh
+# script to configure epon onts
+
+# set required variables
+title="EPON configuration script"
+olt=("gmn-evr-eolt1" "gmn-evr-eolt2" "gmn-pone-eolt1" "Quit")
+service=("data" "datatv" "svlan" "Quit")
+speed=("70" "100" "Quit")
+confirm=("Yes" "No")
+
+# clear screen
+clear
+
+# display script title
+echo -e "\nSelect an OLT"
+
+# set prompt
+PS3="->"
+
+# prompt for olt choice
+select olt_choice in "${olt[@]}"; do
+  case "$olt_choice" in
+    "gmn-evr-eolt1")
+      final_olt="$olt_choice"; break;;
+    "gmn-evr-eolt2")
+      final_olt="$olt_choice"; break;;
+    "gmn-pone-eolt1")
+      final_olt="$olt_choice"; break;;
+    "Quit")
+      echo "Exiting..."; break;;
+    *)
+      echo "Invalid option. Try again."; continue;;
+  esac
+done
+
+# clear screen
+clear
+
+# prompt for olt mac address
+echo -e "\nEnter ONT MAC address: "
+read final_mac
+
+# clear screen
+clear
+
+# prompt for gmn account
+echo -e "\nEnter GMN account: "
+read final_acct
+
+# clear screen
+clear
+
+# set title
+echo -e "\nSelect a service type:"
+
+# set prompt
+PS3="->"
+# prompt for service type
+select service_choice in "${service[@]}"; do
+
+  case "$service_choice" in
+    "data")
+      final_service="$service_choice"; break;;
+    "datatv")
+      final_service="$service_choice"; break;;
+    "svlan")
+      final_service="$service_choice"; break;;
+    "Quit") 
+       echo "Exiting..."; break;;
+     *)
+       echo "Invalid option. Try again."; continue;;
+  esac
+
+done
+  
+# clear screen
+clear
+
+# set title
+echo -e "\nSelect a service speed:"
+
+# set prompt
+PS3="->"
+
+# prompt for service speed
+select speed_choice in "${speed[@]}"; do
+
+  case "$speed_choice" in
+    "70")
+      final_speed="$speed_choice"; break;;
+    "100")
+      final_speed="$speed_choice"; break;;
+    "Quit")
+      echo "Exiting..."; break;;
+    *)
+      echo "Invalid option. Try again."; continue;;
+  esac
+
+done
+
+# clear screen
+clear
+
+# confirm selected command
+echo -e "\nSelected command is \`epoc "$final_olt" "$final_mac" "$final_acct" "$final_service" "$final_speed"\`. Is that correct?"
+
+# set prompt
+PS3="->"
+
+# prompt for final confirmation
+select confirmation in "${confirm[@]}"; do
+
+  case "$confirmation" in
+    "Yes")
+      echo -e "\nConfiguring ONT..."; ssh intranet epoc "$final_olt" "$final_mac" "$final_acct" "$final_service" "$final_speed"; break;;
+    "No")
+      echo "Quitting..."; break;;
+    *)
+      echo "Invalid option. Quitting."; break;;
+  esac
+
+done
