2e02c443703ed3f0a1a5b7ebee05ad1a984dc2c4
Justin Sinc <jpsinkula@hotmail.com>
2017-11-27 00:43:05 -0600

    Create epou_wrapper.sh

---
+++ epou_wrapper.sh
@@ -0,0 +1,56 @@
+#!/bin/rzsh
+# script to deprovision epon onts
+
+# set required variables
+title="EPON deprovisioning script"
+olt=("gmn-evr-eolt1" "gmn-evr-eolt2" "gmn-pone-eolt1" "Quit")
+confirm=("Yes" "No")
+
+# clear screen
+clear
+
+# display script title
+echo -e "\n $title \n"
+
+# set prompt
+PS3="Select an OLT:"
+
+# prompt for olt choice
+select olt_choice in "${olt[@]}"; do
+  case "$olt_choice" in
+    "gmn-evr-eolt1")
+      final_olt="$olt_choice"; break;;
+    "gmn-evr-eolt2")
+      final_olt="$olt_choice"; break;;
+    "gmn-pone-eolt1")
+      final_olt="$olt_choice"; break;;
+    "Quit")
+      echo "Exiting..."; break;;
+    *)
+      echo "Invalid option. Try again."; continue;;
+  esac
+done
+
+# prompt for gmn account
+echo -e "\nEnter ONT MAC address: "
+read ont_mac
+
+# set blank prompt
+PS3=''
+
+# confirm selected command
+echo -e "\nYou're deprovisioning "$ont_mac" on "$olt_choice". Is that correct?\n"
+
+# prompt for final confirmation
+select confirmation in "${confirm[@]}"; do
+
+  case "$confirmation" in
+    "Yes")
+      echo "Deprovisioning ONT..."; ssh intranet epou "$olt_choice" "$ont_mac"; break;;
+    "No")
+      echo "Quitting..."; break;;
+    *)
+      echo "Invalid option. Quitting."; break;;
+  esac
+
+done
