833762b6ab66c922dc8cf35c91280bca7a8c1f6c
Justin Sinc <jpsinkula@hotmail.com>
2018-01-03 07:25:46 -0600

    Update droppedpings.sh

--- droppedpings.sh
+++ droppedpings.sh
@@ -10,5 +10,9 @@ if [ "$#" -ne 1 ];
         exit
 fi
 
-# read file | parse out everything before the icmp_seq value | remove the icmp_seq= string | sort the values numeric$
+# read file
+# parse out everything before the icmp_seq value 
+# remove the icmp_seq= string
+# sort the values numerically
+# display dropped sequences
 cat "$1" | cut -d " " -f5 | cut -d "=" -f2 | sort -V | awk '$1!=p+1{print p+1"-"$1-1}{p=$1}'
