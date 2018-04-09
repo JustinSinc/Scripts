3209d2a7e21d58faa72b7a04ad9f00a252f7a256
Justin Sinc <jpsinkula@hotmail.com>
2018-04-09 14:38:55 -0500

    Update drops.bat

--- drops.bat
+++ drops.bat
@@ -1,5 +1,6 @@
-REM Thanks to user MC ND on stackoverflow.com (https://stackoverflow.com/questions/24906268/ping-with-timestamp) for the original template
 @echo off
+REM Thanks to user MC ND on stackoverflow.com (https://stackoverflow.com/questions/24906268/ping-with-timestamp) for the original template
+REM A script to output timestamped pings to a file on the user's desktop
 IF [%1]==[] echo Usage: drops ^<ip_address^> & exit /b
 IF NOT [%2]==[] echo Too many arguments & exit /b
 ping -t %1|cmd /q /v /c "(pause&pause)>nul & for /l %%a in () do (set /p "data=" && echo(!date! !time! !data!)&ping -n 2 %1>nul" >> %userprofile%\Desktop\droppedpings-%1.txt
